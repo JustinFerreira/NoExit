f08fab85abdd3ef49f422dcd675a48a662319324
JustinFerreira <justin02ferreira@gmail.com>
2025-09-04 19:32:26 -0400

    NoExit Meets OverTime

---
+++ overtime/Scripts/Player.gd
@@ -0,0 +1,75 @@
+extends CharacterBody3D
+
+var speed
+const WALK_SPEED = 1.0
+const SPRINT_SPEED = 1.5
+const JUMP_VELOCITY = 4.5
+const SENSITIVITY = 0.01
+
+#bob variables
+const BOB_FREQ = 2.0 # How often you bob
+const BOB_AMP = 0.08 # How high and low you bob
+var t_bob = 0.0 # Determines how far along the signwave we are for bobbing
+
+#fov variables
+const BASE_FOV = 75.0
+const FOV_CHANGE = 1.5
+
+@onready var head = $Head
+@onready var camera = $Head/Camera3D
+
+# Function that starts as soon as Player in in the scene
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+# Any input that is detected automatically calls this function
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * SENSITIVITY)
+		camera.rotate_x(-event.relative.y * SENSITIVITY)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+
+# Premade Godot Functiuon for movement given to CharacterBody 3D
+# has some added flare for this game
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	##  We don't want a jump so no jumping
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+		
+	# Handle Sprint
+	if Input.is_action_pressed("sprint"):
+		speed = SPRINT_SPEED
+	else:
+		speed = WALK_SPEED
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = 0.0
+		velocity.z = 0.0
+		
+	# Head Bob
+	t_bob += delta * velocity.length() * float(is_on_floor())
+	camera.transform.origin = _headbob(t_bob)
+	
+	# FOV
+	var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
+	var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
+	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
+
+	move_and_slide()
+	
+func _headbob(time) -> Vector3:
+	var pos = Vector3.ZERO
+	pos.y = sin(time * BOB_FREQ) * BOB_AMP
+	pos.x = cos(time * BOB_FREQ / 2) * BOB_AMP
+	return pos
