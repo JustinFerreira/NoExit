8487ea2bbfd3c028325ceefb45078bcf91f73681
JustinFerreira <justin02ferreira@gmail.com>
2025-09-10 19:17:46 -0400

    Main Menu Screen Functional

--- overtime/Scripts/MainMenu.gd
+++ overtime/Scripts/MainMenu.gd
@@ -10,3 +10,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_start_btn_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+
+
+func _on_quit_btn_pressed() -> void:
+	get_tree().quit()
