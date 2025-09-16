c7dfb4ff3580bdfc8195e64e687567631470c8aa
JustinFerreira <justin02ferreira@gmail.com>
2025-09-16 19:10:24 -0400

    Key and Door added

---
+++ overtime/Scripts/PlayerManager.gd
@@ -0,0 +1,33 @@
+extends Node
+
+var Inventory: Array = []
+var MaxWeight: float = 10.0
+var CurrentWeight: float = 0.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func AddToInventory(name: String, weight: float):
+	var item: Dictionary
+	item.name = name
+	item.weight = weight
+	CurrentWeight += weight
+	Inventory.append(item)
+	print(CurrentWeight)
+	
+func RemoveItemByName(name: String) -> bool:
+	for item in Inventory:
+		if item.name == name:
+			CurrentWeight -= item.weight
+			Inventory.erase(item)
+			print(CurrentWeight)
+			return true
+	print(CurrentWeight)
+	return false
