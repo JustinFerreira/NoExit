d5ff3b1e6f4073063271edfd13ca3744775873db
Justin Ferreira <justin02ferreira@gmail.com>
2025-10-30 16:34:10 -0400

    player fog remover

--- overtime/Scripts/main.gd
+++ overtime/Scripts/main.gd
@@ -10,3 +10,5 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	get_tree().call_group("enemy" , "target_position" , target.global_transform.origin)
+	
+	$WorldEnvironment/FogRemover.position = target.position
