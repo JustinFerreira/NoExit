23bea7587e4512c139445f084a53acfec491242a
JustinFerreira <justin02ferreira@gmail.com>
2025-10-30 01:12:09 -0400

    Npcs with Dialog

---
+++ overtime/Scripts/DialogNPC.gd
@@ -0,0 +1,39 @@
+extends Interactable
+
+# Rotation variables
+@export var rotation_speed: float = 2.0  # Adjust this to control turn speed
+var is_rotating: bool = false
+var target_rotation: float = 0.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if is_rotating:
+		# Smoothly rotate toward target rotation
+		var current_rotation = $"../..".rotation.y
+		$"../..".rotation.y = lerp_angle(current_rotation, target_rotation, rotation_speed * delta)
+		
+		# Check if we're close enough to the target rotation
+		if abs(angle_difference(current_rotation, target_rotation)) < 0.05:
+			$"../..".rotation.y = target_rotation
+			is_rotating = false
+
+func _on_interacted(body: Variant) -> void:
+	PlayerManager.CharacterDialog("HELLO FRIEND!", "janitor")
+	
+	# Calculate target rotation (looking away from player)
+	if PlayerManager.player:
+		var current_transform = $"../..".global_transform
+		var target_transform = current_transform.looking_at(PlayerManager.player.global_position, Vector3.UP)
+		target_transform = target_transform.rotated(Vector3.UP, PI)  # Flip 180 degrees
+		
+		# Get the target rotation
+		target_rotation = target_transform.basis.get_euler().y
+		is_rotating = true
+
+# Helper function to calculate angle difference
+func angle_difference(from: float, to: float) -> float:
+	return wrapf(to - from, -PI, PI)
