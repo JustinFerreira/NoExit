3d179baa60e46ef71e0974e980a7778d22838a22
Fellenligerz <114247492+Fellenligerz@users.noreply.github.com>
2025-11-02 19:33:27 -0500

    Added Animated Sliders
    added sprites for the sliders on the settings screen and changed the colors for the menus to red

---
+++ overtime/Scripts/SliderBar.gd
@@ -0,0 +1,10 @@
+extends AnimatedSprite2D
+
+@export var slider: HSlider
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	slider.value_changed.connect(update_sprite)
+	update_sprite(slider.value)
+
+func update_sprite(value: float):
+	frame = max(0, 20 - (value / 5))
