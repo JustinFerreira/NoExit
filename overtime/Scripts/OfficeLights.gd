f5cee868496e3826fb392a693e952ba926940481
Altera-Sefar <135476626+Altera-Sefar@users.noreply.github.com>
2025-11-05 12:04:21 -0500

    started light flickering script - research

---
+++ overtime/Scripts/OfficeLights.gd
@@ -0,0 +1,17 @@
+extends OmniLight3D
+
+@export var noise: NoiseTexture2D
+var timePassed := 0.0
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	timePassed += delta
+	
+	var sampledNoise = noise.noise.get_noise_1d(timePassed * 5)
+	light_energy = abs(sampledNoise * 10)
+	print(light_energy)
+	pass
